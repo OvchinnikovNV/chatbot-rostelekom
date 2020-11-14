8c86973d73d33af918a4638c789e1493156b04fd
OvchinnikovNV <bugaga.dada@mail.ru>
2020-11-14 16:19:51 +0300

    Add app.py: front + back

--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 @app.route("/get")
 def neural_network():
-    pass
+    user_text = request.args.get('msg')
+    return str(user_text)
+
 
 
 if __name__ == '__main__':
